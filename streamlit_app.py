c2a54318a6bb61b5af55e798e21365264bfa5371
omkar-joglekar <128417587+omkar-joglekar@users.noreply.github.com>
2023-03-21 08:44:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,28 +46,38 @@ rows4 = run_query("select sum(sp_f) from SCOREBOARD_MAR2023 where type='FDN';")
 df4=pd.DataFrame(rows4)
 df4[0] = df4[0].astype(int)
 
-col1, col2 = st.columns([1,2])
+rows5 = run_query("select TEAM__GOOGLE_SHEETS, MARCH_AGENTS__GOOGLE_SHEETS, sum(SP_F__GOOGLE_SHEETS) from RAW_FUNNEL_SPRING_PROD_DB.FUNNEL__MCBJDTJ0LVHQIF12J1F.TEAMLEADS_MAR2023 where TYPE__GOOGLE_SHEETS='EFS' group by TEAM__GOOGLE_SHEETS, MARCH_AGENTS__GOOGLE_SHEETS order by 1;")
+df5=pd.DataFrame(rows5)
+df5.columns += 1
+df5.index = df5.index + 1
+df5.columns = ["Team", "Lead", "Funded"]
+df5['Funded'] = df5['Funded'].astype(int)
+
+rows6 = run_query("select TEAM__GOOGLE_SHEETS, MARCH_AGENTS__GOOGLE_SHEETS, sum(SP_F__GOOGLE_SHEETS) from RAW_FUNNEL_SPRING_PROD_DB.FUNNEL__MCBJDTJ0LVHQIF12J1F.TEAMLEADS_MAR2023 where TYPE__GOOGLE_SHEETS='FDN' group by TEAM__GOOGLE_SHEETS, MARCH_AGENTS__GOOGLE_SHEETS order by 1;")
+df6=pd.DataFrame(rows6)
+df6.columns += 1
+df6.index = df6.index + 1
+df6.columns = ["Team", "Lead", "Funded"]
+df6['Funded'] = df6['Funded'].astype(int)
+
+col1, col2 = st.columns([4,4])
 
 with col1:
    st.subheader('Total EFS Funded')
    st.metric("", df3[0])
-   st.text('Team 1:')
-   st.text('Team 2:')
-   st.text('Team 3:')
+   st.dataframe(df5)
 
 with col2:
    st.header('Top EFS Agents')
    st.dataframe(df)
 
 
-col3, col4 = st.columns([1,2])
+col3, col4 = st.columns([4,4])
 
 with col3:
    st.subheader('Total FDN Funded')
    st.metric("", df4[0])
-   st.text('Team 1:')
-   st.text('Team 2:')
-   st.text('Team 3:')
+   st.dataframe(df6)
 
 with col4:
    st.header('Top FDN Agents')
