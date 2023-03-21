a527cdb22e56a7addf13d66dc206c3bea17b8c7e
omkar-joglekar <128417587+omkar-joglekar@users.noreply.github.com>
2023-03-21 08:28:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,13 +37,38 @@ df2.index = df2.index + 1
 df2.columns = ["Agent Name", "Funded"]
 df2['Funded'] = df2['Funded'].astype(int)
 
+rows3 = run_query("select sum(sp_f) from SCOREBOARD_MAR2023 where type='EFS';")
+df3=pd.DataFrame(rows3)
+#df3[0] = df3[0].astype(int)
+df3[0] = df3[0].replace('',',').astype(int)
 
-col1, col2 = st.columns([4,3])
+rows4 = run_query("select sum(sp_f) from SCOREBOARD_MAR2023 where type='FDN';")
+df4=pd.DataFrame(rows4)
+df4[0] = df4[0].astype(int)
+
+col1, col2 = st.columns([1,4])
 
 with col1:
-   st.header('Top EFS agents')
-   st.dataframe(df)
+   st.subheader('Total EFS Funded')
+   st.metric("", df3[0])
+   st.text('Team 1:')
+   st.text('Team 2:')
+   st.text('Team 3:')
 
 with col2:
+   st.header('Top EFS Agents')
+   st.dataframe(df)
+
+
+col3, col4 = st.columns([1,4])
+
+with col3:
+   st.subheader('Total FDN Funded')
+   st.metric("", df4[0])
+   st.text('Team 1:')
+   st.text('Team 2:')
+   st.text('Team 3:')
+
+with col4:
    st.header('Top FDN Agents')
    st.dataframe(df2)
