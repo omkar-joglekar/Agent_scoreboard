45f4f81d5d356fd8bf891db3bffe575cf3a6400a
omkar-joglekar <128417587+omkar-joglekar@users.noreply.github.com>
2023-03-24 11:30:49 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -156,5 +156,5 @@ with tab3:
           st.table(df7)
           
 st.caption('_Updates every 2 hours_')
-st.write("Next data refresh at:", next_refresh.strftime("%Y-%m-%d %I:%M %p"))
+st.write("Next data refresh at:", next_refresh.strftime("%I:%M %p"))
 st.write(now)
