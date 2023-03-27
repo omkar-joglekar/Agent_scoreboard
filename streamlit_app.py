7dcb73c22a6a459f751c0f9da9e24f37d9fa0a2a
omkar-joglekar <128417587+omkar-joglekar@users.noreply.github.com>
2023-03-27 07:53:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,7 +155,7 @@ with tab3:
   
    with col5:
           st.subheader('Total CSR Decline Funded')
-          st.metric("label1",df9[0], label_visibility="collapsed")
+          st.metric("label1",df9['Total_DF'], label_visibility="collapsed")
           st.markdown(hide_table_row_index, unsafe_allow_html=True)
           st.table(df8)
 
