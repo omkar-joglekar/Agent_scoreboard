8b29469946fbaeb788736e4998ebad71503f8d94
omkar-joglekar <128417587+omkar-joglekar@users.noreply.github.com>
2023-03-27 10:12:57 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -165,20 +165,20 @@ with tab3:
           st.table(df7)
        
     
-#display the next refresh time 
-    if hours == 0:
-    st.write(f"Next refresh in {minutes} minute{'s' if minutes != 1 else ''} ({next_refresh_time})")
-    else:
-    st.write(f"Next refresh in {hours} hour{'s' if hours != 1 else ''} {minutes} minute{'s' if minutes != 1 else ''} ({next_refresh_time})")
+    #display the next refresh time 
+    #if hours == 0:
+    #st.write(f"Next refresh in {minutes} minute{'s' if minutes != 1 else ''} ({next_refresh_time})")
+    #else:
+    #st.write(f"Next refresh in {hours} hour{'s' if hours != 1 else ''} {minutes} minute{'s' if minutes != 1 else ''} ({next_refresh_time})")
     
 col7, col8, col9, col10, col11 = st.columns(5)
 
 with col7:
     #display the next refresh time 
-    if hours == 0:
-        st.write(f"Next refresh in {minutes} minute{'s' if minutes != 1 else ''} ({next_refresh_time})")
-    else:
-        st.write(f"Next refresh in {hours} hour{'s' if hours != 1 else ''} {minutes} minute{'s' if minutes != 1 else ''} ({next_refresh_time})")
+  if hours == 0:
+    st.write(f"Next refresh in {minutes} minute{'s' if minutes != 1 else ''} ({next_refresh_time})")
+  else:
+    st.write(f"Next refresh in {hours} hour{'s' if hours != 1 else ''} {minutes} minute{'s' if minutes != 1 else ''} ({next_refresh_time})")
     
 with col8:
     st.write("")
