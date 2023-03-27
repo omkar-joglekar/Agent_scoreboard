f8435e013244a2bde0580b523ebb4ab4ea38bc4e
omkar-joglekar <128417587+omkar-joglekar@users.noreply.github.com>
2023-03-27 10:17:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,7 +171,7 @@ with tab3:
     #else:
     #st.write(f"Next refresh in {hours} hour{'s' if hours != 1 else ''} {minutes} minute{'s' if minutes != 1 else ''} ({next_refresh_time})")
     
-col7, col8, col9, col10, col11 = st.columns([1.5,0.5,0.5,0.5,1.5])
+col7, col8, col9, col10, col11, col12 = st.columns([1.5,0.5,0.5,0.5,0.5,1.5])
 
 with col7:
     #display the next refresh time 
@@ -187,4 +187,6 @@ with col9:
 with col10:
     st.write("")
 with col11:
+    st.write("")
+with col12:
     st.image("logo.png")
