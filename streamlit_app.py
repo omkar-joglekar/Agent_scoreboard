7969028f07c864be9f74332bf797a61047b4a715
omkar-joglekar <128417587+omkar-joglekar@users.noreply.github.com>
2024-05-01 10:22:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -356,7 +356,7 @@ formatted_dates = pd.to_datetime(dates).dt.strftime('%B %Y').unique()
 def custom_sort(date):
     month_order = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     month, year = date.split()
-    return int(year), month_order.index(month)
+    return int(year), -month_order.index(month)
 
 # Sort the formatted dates using the custom sorting key function
 sorted_dates = sorted(formatted_dates, key=custom_sort, reverse=True)  # Reverse the sorting order
